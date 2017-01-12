57862ce8e197fd40aea846b654b2d35a3ba0e817
dsoheili <dansoheili@gmail.com>
2017-01-11 22:25:15 -0800

    complete part 1

---
+++ script.py
@@ -0,0 +1,46 @@
+iterationsLeft = 40
+inputString = "1113122113"
+#Solution: 360154 is the length of the result
+
+def say(phrase, lastValCount, lastVal):
+    phrase = phrase + str(lastValCount) + str(lastVal)
+    lastVal = currentVal
+    lastValCount = 1
+
+def checkValues(currentVal, lastValCount, lastVal, index):
+    print "currentVal:" + str(currentVal) + ", lastVal:" + str(lastVal) + ", lastValCount:" + str(lastValCount) + ", index:" + str(index)
+
+while (iterationsLeft > 0):
+    lastVal = -1
+    lastValCount = -1
+    phrase = ""
+    index = 1
+    for currentVal in inputString:
+
+        #checkValues(currentVal, lastValCount, lastVal, index)
+
+        #Stage 1: Empty check
+        if (lastVal == -1):
+            lastVal = currentVal
+            lastValCount = 1
+
+        #Stage 2: Value check
+        elif (currentVal == lastVal):
+            lastValCount += 1
+
+        elif (currentVal != lastVal):
+            phrase = phrase + str(lastValCount) + str(lastVal)
+            lastVal = currentVal
+            lastValCount = 1
+
+        #Stage 3: End check
+        if (index == len(inputString)):
+            phrase = phrase + str(lastValCount) + str(lastVal)
+            lastVal = currentVal
+            lastValCount = 1
+
+        index += 1
+
+    iterationsLeft -= 1
+    inputString = phrase
+    print len(phrase)
